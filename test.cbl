ac26cdbd92af605bb07464eb9b9b30d7be903ebb
amour-youcef <amour-youcef@hotmail.com>
2023-02-26 12:27:52 +0100

    26022023

---
+++ test.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.      
+       PROGRAM-ID 'BIENVENU'.        
+       AUTHOR. YOUSSOUF.             
+      *THIS IS A COMMENT IN COBOL    
+       ENVIRONMENT DIVISION.         
+       DATA DIVISION.                 
+       PROCEDURE DIVISION.           
+         0100-START-HERE.             
+           STOP RUN.                
+           END PROGRAM OBTAININPUT.  
+/*                                   
+//                                   
---
+++ variable.cbl
@@ -0,0 +1,9 @@
+       identification division.
+       program-id. VARIABLE.
+       author. youssouf.
+
+       procedure division.
+       
+           display "hello world".
+
+           stop run. 
