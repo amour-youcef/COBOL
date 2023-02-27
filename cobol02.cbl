cef11d0adee2ba5577fccd36d4d26fc39d7d2ac0
amour-youcef <amour-youcef@hotmail.com>
2023-02-27 12:05:57 +0100

    270220232

--- cobol02.cbl
+++
@@ -1,9 +0,0 @@
-       identification division.
-       program-id. helloworld.
-       author. youssouf.
-
-       procedure division.
-       
-       display "hello world".
-
-           stop run. 
