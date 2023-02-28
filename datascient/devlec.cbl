33cd2179e47eee357ef94244728a3c0ebb95bb08
amour-youcef <amour-youcef@hotmail.com>
2023-02-28 13:13:51 +0100

    28022023

--- datascient/devlec.cbl
+++ datascient/devlec.cbl
@@ -1,12 +1,13 @@
        IDENTIFICATION DIVISION.            
-       PROGRAM-ID. DEVLEC.                 
+       PROGRAM-ID. DEVLEC.  
+
        ENVIRONMENT DIVISION.               
        INPUT-OUTPUT SECTION.               
        FILE-CONTROL.                       
-       SELECT EMPLOYE ASSIGN TO OUT1   
-       ORGANIZATION IS SEQUENTIAL    
-       ACCESS IS SEQUENTIAL        
-            FILE STATUS IS FS.    
+           SELECT EMPLOYE ASSIGN TO OUT1   
+           ORGANIZATION IS SEQUENTIAL    
+           ACCESS IS SEQUENTIAL .       
+      *      FILE STATUS IS FS.    
    
        DATA DIVISION.                            
        FILE SECTION.                             
@@ -15,18 +16,18 @@
            05 ID-EMPLOYE   PIC A(15).            
            05 NAME-EMPLOYE PIC A(15).            
            05 TITRE-EMPLOYE PIC X(3).            
-       WORKING-STORAGE SECTION.                  
-       01 WS-EMPLOYE.                            
-           05 WS-EMPLOYE-ID  PIC 9(3).           
-           05 WS-NAME-EMPLOYE PIC A(15).         
-           05 WS-TITRE-EMPLOYE PIC X(3).         
+      * WORKING-STORAGE SECTION.                  
+      * 01 WS-EMPLOYE.                            
+      *     05 WS-EMPLOYE-ID  PIC 9(3).           
+      *     05 WS-NAME-EMPLOYE PIC A(15).         
+      *     05 WS-TITRE-EMPLOYE PIC X(3).         
        PROCEDURE DIVISION.                       
            OPEN EXTEND EMPLOYE.                  
            MOVE '560' TO ID-EMPLOYE.          
            MOVE 'JEAN' TO NAME-EMPLOYE.       
            MOVE '120'  TO TITRE-EMPLOYE.      
-           WRITE ENR-EMPLOYE.                 
-           END-WRITE.                         
+           WRITE ENR-EMPLOYE                 
+           END-WRITE.                       
            CLOSE EMPLOYE.  
 
            STOP RUN.                      
