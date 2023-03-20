c737d5624f37b04738d3c03d7a7b2d6ade2e0f75
amour-youcef <amour-youcef@hotmail.com>
2023-03-20 12:17:58 +0100

    20032023

---
+++ codeweare/TEST/HELLO.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "HELLOWORLD".
+       AUTHOR.     PEGGY FISHER.
+      *This is a comment in COBOL
+      *Columns 8-11 A Margin
+      *Columns 12-72 B Margin
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION. 
+        0100-START-HERE.
+           DISPLAY "Hello World! codeweare".
+           STOP RUN.
+       END PROGRAM HELLOWORLD.
