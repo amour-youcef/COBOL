d6d099858ac2cadd84517261f1c72bbaf92c5e5a
amour-youcef <amour-youcef@hotmail.com>
2023-03-23 11:02:36 +0100

    23032023

--- CICS/PRGCICS4.cbl
+++ CICS/PRGCICS4.cbl
@@ -15,6 +15,35 @@
            05 WS-PNOM      PIC X(15).
       01 WS-LENGTH-AFF   PIC S9(4) COMP.
 
+       01 MAP1I.
+           05 FILLER PIC X(12).
+           05 TRTML PIC S9(04) COMP.
+           05 TRTMF PIC X.
+           05 TRTMI PIC X(9).
+           05 DATEL PIC S9(04) COMP.
+           05 HEUREL PIC S9(04) COMP  .
+           05 HEUREF PIC X.
+           05 HEUREI PIC X(08) COMP.
+           05 CHOIXL PIC S9(04) COMP .
+           05 CHOIXF PIC X.
+           05 CHOIXI PIC X(01).
+           05 MESSAGEL PIC X S9(04) COMP.
+           05 MESSAGEF PIC X.
+           05 MESSAGEI PIC  X(65). 
+       01 MAP1O REDEFINES MAP1I.
+           05 FILLER PIC X(12).préfixe TIOA
+           05 FILLER PIC XX.
+           05 NOMA PIC X.
+           05 NOMO PIC X(20).
+           05 FILLER PIC XX.
+           05 NUMEROA PIC X.
+           05 NUMEROO PIC X(05).
+           05 FILLER PIC XX.
+           05 SOLDEA PIC X.
+           05 SOLDEO PIC ZZZ9,99.
+           05 FILLER PIC XX.
+           05 MESSAGEA PIC X.
+           05 MESSAGEO PIC X(79).
        PROCEDURE DIVISION.
        0000-MAIN-PARA.
       * PREMIER AFFICHAGE
