9566823c519e75aab9c9a2ad116b621a55ea62fc
amour-youcef <amour-youcef@hotmail.com>
2023-04-11 13:15:07 +0200

    "okkk"

--- PROJET_COBOL/PARTI14/pgsecond.cbl
+++ PROJET_COBOL/PARTI14/pgsecond.cbl
@@ -6,16 +6,16 @@
         OBJECT-COMPUTER. IBM-ZOS.       
         INPUT-OUTPUT SECTION. 
         FILE-CONTROL. 
-            SELECT INPUT-FILE ASSIGN TO 'INPUT.FILE'.
-            SELECT OUTPUT-FILE ASSIGN TO 'OUTPUT.FILE'.
+            SELECT FIL-REGION ASSIGN TO 'REGION'.
+            SELECT OUT-REGION ASSIGN TO 'DDPRINT'.
         DATA DIVISION.
         FILE SECTION. 
         
         WORKING-STORAGE SECTION.   
-        FD fprint.                       
-        01 LS-LENGHT   PIC  s9(4) COMP .
-        01 LS-REGION-ID PIC  x(2).
-        01 LS-NOM-region.                                                                               
+        FD FIL-REGION.                       
+        01 REGION-RECORD.
+           02 REGION-ID    PIC  9(2).
+           02 NOM-REGION   PIC  x(15).                                                          
         LINKAGE SECTION. 
         01 page-ctr     pic 9(3) VALUE 1.
         01 LINE-ctr     PIC  9(3) VALUE 1.
@@ -25,14 +25,13 @@
            02 WSID-REGION-id  PIC  x(2).
            02 ws-nom-region  PIC x(15). 
 
-       01 REGION-record.
-           02 region-id    PIC  9(2).
-           02 nom-region   PIC  x(15).
+       
                     
         PROCEDURE DIVISION USING ID-REGION  NOM-REGION.
         0100-BEGIN.       
-           OPEN INPUT INPUT-FILE.
-           OPEN OUTPUT OUTPUT-FILE
+           OPEN INPUT FIL-REGION.
+      *     OPEN OUTPUT OUTPUT-FILE
+           READ  FIL-REGION AT END SET 
            PERFORM  0200-PROCESS-RECORDS.
            PERFORM print-region UNTIL END-OF-END.
            CLOSE INPUT-FILE.
